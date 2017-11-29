d96d72cd81a9885cd4dcc8afc6d2ec68aaea92f6
Parker Sarsfield <parker.h.sarsfield@vanderbilt.edu>
2017-11-28 21:57:39 -0600

    Add first pass of solver.

--- sudoku.go
+++ sudoku.go
@@ -31,7 +31,38 @@ func print(puzzle Puzzle) {
 	}
 }
 
-//solves the sudoku puzzle from 
+//solves the sudoku puzzle from i, j and returns true if it is solved, false if it cannot be solved.
 func solve(i, j int, puzzle Puzzle) bool {
-	return false
+	if i == 9 {
+		i = 0
+		j++
+		
+		if j == 9 {
+			return true
+		}
+	}
+
+	// solve the blank cell
+	if puzzle[i][j] == 0 {
+		for testNum := 1; testNum <= 9; testNum++ {
+			if valid(testNum, i, j) {
+				puzzle[i][j] = testNum
+				if (solve(i + 1, j, puzzle)) {
+					return true
+				}
+			}
+		}
+
+		// Tried all 9 numbers. Reset it to 0 and backtrack.
+		puzzle[i][j] = 0
+		return false
+	}
+
+	// Space was already filled.
+	return solve(i + 1, j, puzzle)
+}
+
+func valid(x, i, j int) bool {
+	// NOT IMPLEMENTED
+	return true;
 }
